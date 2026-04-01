0a1cf618f9f0c5747832274818508057c06634b7
ray <raychen09060@gmail.com>
2026-04-01 13:31:50 +0800

    ImagePicker final release

--- HW/wk5/app/index.jsx
+++ HW/wk5/app/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image, Button, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView, Pressable } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { pickImage } from '../components/ImagePicker';
@@ -6,24 +6,34 @@ import Header from '../components/Header';
 
 
 export default function Home() {
-  const [photo, setPhoto] = React.useState(null);
+  const [photos, setPhotos] = React.useState([]);
   const handleSelectPhoto = async () => {
     const base64String = await pickImage();
-    setPhoto(base64String);
+    setPhotos(prevPhotos => [...prevPhotos, base64String]);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <Header/>
       <View style={styles.gallery_container}>
-        <Pressable style={styles.upload_button} onPress={handleSelectPhoto}>
-          <Text style={styles.upload_button_text}>Upload Images</Text>
-        </Pressable>
-        <View style={styles.image_container}>
-          {photo && (
-            <Image source={{ url: photo }} resizeMode="cover" />
-          )}
+        <View style={styles.button_container}>
+          <Pressable style={({ pressed }) => [styles.upload_button, { backgroundColor: pressed ? '#a0a0a0' : '#ffffff' }]} onPress={handleSelectPhoto}>
+            <Text style={styles.upload_button_text}>Upload</Text>
+          </Pressable>
+          <Pressable style={({ pressed }) => [styles.upload_button, { backgroundColor: pressed ? '#a0a0a0' : '#ffffff' }]} onPress={() => setPhotos([])}>
+            <Text style={styles.upload_button_text}>Clear</Text>
+          </Pressable>
         </View>
+        <ScrollView style={styles.images_container}>
+          {photos.map((photo, index) => (
+            <Image 
+              key={index}
+              source={{ uri: photo }} 
+              resizeMode="cover" 
+              style={styles.images} 
+            />
+          ))}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -38,13 +48,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
   },
+  button_container: {
+    width: 300,
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 20,
+  },
   upload_button: {
-    width: 200,
+    width: 100,
     height: 40,
     backgroundColor: "#ffffff",
     justifyContent: "center",
     alignItems: "center",
-    marginVertical: 20,
     borderWidth: 2,
     borderColor: "#000000",
     borderRadius: 20,
@@ -53,11 +69,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
   },
-  image_container: {
+  images_container: {
+    flex: 1,
+    },
+  images: {
     width: 300,
     height: 200,
-    backgroundColor: "#e0e0e0",
-    borderRadius: 30,
-    },
+    marginBottom: 20,
+    borderRadius: 10,
+  },
 });
 
